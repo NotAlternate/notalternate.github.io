d6ed21f9ce18477186eb68a4a89e3b9ce92ea2e4
NotAlternate <alternateglitchy@gmail.com>
2022-07-26 16:53:21 +0700

    Added redirection for deprecated link & fixed anchor link

--- hexagn/asset/js/default.js
+++ hexagn/asset/js/default.js
@@ -1,19 +1,32 @@
-window.onload = function() { 
+var page = {};
+window.onload = function() {
+    page.original = document.body.innerHTML;
     if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-        if(screen.availHeight > screen.availWidth) {
+        if(window.matchMedia("(orientation: portrait)").matches) {
             document.body.innerHTML = `
             <div style="padding:20px;">
-                In order to view this website in <b>Mobile</b>, Please switch to landscape mode and then reload the webpage to view.<br><br>
+                In order to view this website in <b>Mobile</b>, Please switch to landscape mode.<br><br>
                 Viewing in portrait mode can lead to unexpected layout.
             </div>`;
             document.getElementsByTagName("html")[0].style.visibility = "visible";
-        } else {
-            document.getElementsByTagName("html")[0].style.visibility = "visible";
-            document.getElementById("link").id = "mobile";
-        }
-    } else
-        document.getElementsByTagName("html")[0].style.visibility = "visible";
+        } else document.getElementsByTagName("html")[0].style.visibility = "visible"; 
+            /*document.getElementById("link").id = "mobile";*/
+    } else document.getElementsByTagName("html")[0].style.visibility = "visible";
 };
+window.onresize = function () {
+    if(window.matchMedia("(orientation: portrait)").matches) {
+        document.body.innerHTML = `
+        <div style="padding:20px;">
+            In order to view this website in <b>Mobile</b>, Please switch to landscape mode.<br><br>
+            Viewing in portrait mode can lead to unexpected layout.
+        </div>`;
+        document.getElementsByTagName("html")[0].style.visibility = "visible";
+    } else {
+        document.body.innerHTML = page.original;
+        document.getElementsByTagName("html")[0].style.visibility = "visible";
+        /*document.getElementById("link").id = "mobile";*/
+    }
+}
 /*
 window.onscroll = function() {
     if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) return;
